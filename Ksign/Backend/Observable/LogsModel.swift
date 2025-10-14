72cd47cf5618bde2fda9bb05c703ccb4cccb68bb
Nyasami <80683081+Nyasami@users.noreply.github.com>
2025-10-14 18:37:50 +0700

    chore: remove timestamp for entries

--- Ksign/Backend/Observable/LogsModel.swift
+++ Ksign/Backend/Observable/LogsModel.swift
@@ -5,15 +5,8 @@
 //  Created by Nagata Asami on 8/10/25.
 //
 
-import Foundation
 
 struct LogEntry: Identifiable, Equatable {
     let id = UUID()
     let message: String
-    let timestamp: Date
-
-    init(message: String, timestamp: Date = Date()) {
-        self.message = message
-        self.timestamp = timestamp
-    }
 }
